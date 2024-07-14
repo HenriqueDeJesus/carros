ce244f3260e9b63e0124c7ee84ba29b331d9497b
Henrique Guilherme de Jesus Hernandes <136270343+HenriqueDeJesus@users.noreply.github.com>
2024-07-14 20:15:30 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carros',
         'USER': 'postgres',
-        'PASSWORD': 'Hery980$',
+        'PASSWORD': 'Django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
